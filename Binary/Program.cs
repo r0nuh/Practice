cc621b521e00ab77ee01dc647e6a05d32fbe8a9b
r0nuh <cs.szilard@gmail.com>
2018-07-06 11:03:50 +0200

    feat(Practice): create binary number

---
+++ Binary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a positive number: ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.Write($"The binary equivalent of {n} is: ");
+            foreach (var item in ToBinary(n))
+            {
+                Console.Write(item);
+            }
+            
+            Console.Read();
+        }
+
+        static List<int> ToBinary(int n)
+        {
+            List<int> binary = new List<int>();
+            int digit;
+
+            while (n > 0)
+            {
+                digit = n % 2;
+                binary.Insert(0, digit);
+                n /= 2;
+            }
+
+            return binary;
+        }
+    }
+}
