1704593d36575e1aa0624879b7d661abfaaf804e
r0nuh <cs.szilard@gmail.com>
2018-07-09 21:22:18 +0200

    feat(Practice): add Hourglass and LinkedListInsertFunc exercises

---
+++ Hourglass/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Hourglass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr =
+            {
+                new int[] {1, 1, 1, 0, 0, 0},
+                new int[] {0, 1, 0, 0, 0, 0},
+                new int[] {1, 1, 1, 0, 0, 0},
+                new int[] {0, 0, 2, 4, 4, 0},
+                new int[] {0, 0, 0, 2, 0, 0},
+                new int[] {0, 0, 1, 2, 4, 0}
+            };
+
+            Console.WriteLine(HourglassSum(arr));
+            Console.Read();
+        }
+
+        static int HourglassSum(int[][] arr)
+        {            
+            int max = 0;
+            int sum = 0;
+            int startI = 0;
+            int endI = 3;
+            int startJ = 0;
+            int endJ = 3;
+
+            while (endI <= 6)
+            {
+                for (int i = startI; i < endI; i++)
+                {
+                    if (i == startI + 1)
+                    {
+                        sum += arr[i][endJ - 2];
+                    }
+                    else
+                    {
+                        for (int j = startJ; j < endJ; j++)
+                        {
+                            sum += arr[i][j];
+                        }
+                    }
+                }
+                if (endJ < 6)
+                {
+                    ++startJ;
+                    ++endJ;
+                }
+                else
+                {
+                    startJ = 0;
+                    endJ = 3;
+                    ++startI;
+                    ++endI;
+                }
+                
+                max = Math.Max(max, sum);                
+                sum = 0;
+            }
+            return max;
+        }
+    }
+}
---
+++ LinkedListInsertFunc/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LinkedListInsertFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node head = null;
+            Console.Write("Enter how many numbers you want to insert: ");
+            int T = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Numbers to be inserted: ");
+
+            while (T-- > 0)
+            {                
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            display(head);
+            Console.Read();
+
+        }
+
+        public static Node insert(Node head, int data)
+        {
+            Node newNode = new Node(data);
+            Node current = head;
+
+            if (head != null)
+            {
+                current.next = insert(head.next, data);
+            }
+            else
+            {
+                head = newNode;
+            }
+
+            return head;
+        }
+
+        public static void display(Node head)
+        {
+            Node start = head;
+            Console.WriteLine("Linked list elements: ");
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+
+    }
+}
